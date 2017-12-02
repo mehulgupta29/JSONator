2b6b64640b5a6dbcfe1de64a86aa9d2f6111fcb9
Mehul Gupta <MehulGupta@Mehuls-MBP.fios-router.home>
2017-12-02 15:33:05 -0500

    Added FileLoader

--- src/components/fileLoader/FileLoader.jsx
+++ src/components/fileLoader/FileLoader.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from 'react';
+import Dropzone from 'react-dropzone';
+import { each, map } from 'lodash';
 
-export default class FileLoader extends React.Component {
+class FileLoader extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            accepted: [],
+            rejected: [],
+            fileContents: null
+        }
+    }
+
+    parser(file) {
+        let self = this;
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            // const result = JSON.parse(e.target.result);
+            // const formatted = JSON.stringify(result, null, 2);
+            //
+            // function checkProperties(property, object) {
+            //     property in object ? console.log('Child detected') : missingProps.push(property);
+            // }
+            // checkProperties('keyExists', result);
+            // checkProperties('keyNotExist', result);
+            console.log(reader.result);
+            self.setState({fileContents: reader.result});
+        };
+        reader.readAsText(file);
+    }
+
     render () {
         return (
             <div>
                 File Loader
+                <section>
+                    <div className="dropzone">
+                        <Dropzone
+                            accept="application/json"
+                            onDrop={(accepted, rejected) => { this.setState({ accepted, rejected });}}
+                        >
+                            <p>Try dropping some files here, or click to select files to upload.</p>
+                            <p>Only *.json files will be accepted</p>
+                        </Dropzone>
+                    </div>
+                    <aside>
+                        <p>Accepted files</p>
+                        <ul>
+                            {
+                                this.state.accepted.map(f => <button key={f.name} onClick={(event) => {this.parser(f)}}>{f.name} - {f.size} bytes</button>)
+                            }
+                        </ul>
+                        <p>Rejected files</p>
+                        <ul>
+                            {
+                                this.state.rejected.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+                            }
+                        </ul>
+                    </aside>
+                </section>
+                <section>
+
+                </section>
             </div>
         )
     }
 }
+
+export default FileLoader;
